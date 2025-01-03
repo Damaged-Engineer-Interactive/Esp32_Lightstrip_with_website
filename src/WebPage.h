d90d4738650b16fc7c1947c237f65d21a0309a7d
Aarion <aaron.oelschlegel@outlook.com>
2025-01-03 21:00:13 +0100

    Initial commit

---
+++ src/WebPage.h
@@ -0,0 +1,316 @@
+#ifndef WebPage_h
+#define WebPage_h
+
+#include <Arduino.h>
+#include <WebServer.h>
+#include <Update.h>
+#include "NVMData.h"
+#include "DynamicData.h"
+
+class WebPage
+{
+private:
+    WebServer server;
+    String GenHeader(int redirectTime);
+    String GenFooter();
+    String GenTableStart();
+    String GenTableNewColumn();
+    String GenTableRows(String Content[], int Count);
+    String GenTableEnd();
+    String addKeyValuePair(String key, String value);
+    void handleRoot();
+    void handleChange();
+    void handleNotFound();
+    void handleFirmware();
+    void handleUpload();
+    void handleUpload2();
+    void handlePowerSource();
+    void handlePowerSource2();
+    void handleJson();
+    String setMessage(String arg, String value, String result);
+public:
+    WebPage();
+    ~WebPage();
+    void Init();
+    void loop();
+    bool newNetworkSet = false;
+};
+WebPage::WebPage()
+{
+}
+WebPage::~WebPage()
+{
+}
+WebServer server(80);
+void WebPage::loop() {
+    server.handleClient();
+}
+void WebPage::Init() {
+    
+    server.on("/", std::bind(&WebPage::handleRoot, this));
+    server.on("/change", std::bind(&WebPage::handleChange, this));
+    server.on("/json", std::bind(&WebPage::handleJson, this));
+    // choose bin file
+    server.on("/firmware", HTTP_GET, std::bind(&WebPage::handleFirmware, this));
+    /*handling uploading firmware file */
+    server.on("/update", HTTP_POST, std::bind(&WebPage::handleUpload, this), std::bind(&WebPage::handleUpload2, this));
+    server.onNotFound(std::bind(&WebPage::handleNotFound, this));
+    server.begin();
+    Serial.println("HTTP server started");
+}
+String WebPage::GenHeader(int redirectTime)
+{
+  Serial.println(redirectTime);
+  String message= "";
+  message += "<html>";
+  message += "<head>";
+  if (redirectTime > 0)
+  {
+    String redirectTimeString = String(redirectTime);
+    message += "<meta http-equiv=\"refresh\" content=\"" + redirectTimeString + ";url=http://" + DynamicData::get().ipaddress + "/\" />";
+  }
+  message += "</head>";
+  message += "<body>\n";
+  message += "\t<p>This will only work until it breaks</p>\n";
+  message += "\t<p>------------V0.1.0---------------</p>\n";
+  return message;
+}
+String WebPage::GenFooter()
+{
+  String message= "";
+  message += "\t<p>---------------------------</p>\n";
+  message += "</body>";
+  message += "</html>\n";
+  return message;
+}
+String WebPage::GenTableStart()
+{
+  String message= "";
+  message += "\t<table border=\"4\">";
+  message += "\t<tr>\n";
+  return message;
+}
+String WebPage::GenTableNewColumn()
+{
+  String message= "";
+  message += "\t</tr>";
+  message += "\t<tr>\n";
+  return message;
+}
+String WebPage::GenTableRows(String Content[], int Count)
+{
+  String message = "";
+  for (int i = 0; i < Count; i++)
+  {
+    message += "<td>"+Content[i]+"</td>";
+  }
+  message +="\n";
+  return message;
+}
+String WebPage::GenTableEnd()
+{
+  String message= "";
+  message += "\t</tr>";
+  message += "\t</table>\n";
+  return message;
+}
+void WebPage::handleNotFound() {
+  String message= "";
+  message += GenHeader(3);
+  message += "404: Listen, I'm just as confused as you are okay?\n\n";
+  message += "How did you get here anyways? Just go back to the home page okay?\n\n";
+  message += GenFooter();
+  server.send(200, "text/html", message);
+}
+void WebPage::handleChange() {
+  String message = "Ohh oh!\n\n";
+  bool netNameSet = false;
+  bool netPasswordSet = false;
+  String netName = "";
+  String netPassword = "";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    if (server.argName(i) == "netname")
+    {
+      netNameSet = true;
+      netName = server.arg(i);
+    }
+    else if (server.argName(i) == "password")
+    {
+      netPasswordSet = true;
+      netPassword = server.arg(i);
+    }
+    else if (server.argName(i) == "scene") {
+      DynamicData::get().scene =  server.arg(i).toInt();
+      message = "You got it!";
+    }
+    else if (server.argName(i) == "red") {
+      DynamicData::get().red =  server.arg(i).toInt();
+      message += "red";
+    }
+    else if (server.argName(i) == "green") {
+      DynamicData::get().green =  server.arg(i).toInt();
+      message += "green";
+    }
+    else if (server.argName(i) == "blue") {
+      DynamicData::get().blue =  server.arg(i).toInt();
+      message += "blue";
+    }
+  }
+  if ((netPasswordSet == true)&&(netNameSet == true))
+  {
+    newNetworkSet = true;
+    NVMData::get().SetNetData(netName, netPassword);
+    message = "You got it!";
+  }
+  String returnMessage= "";
+  returnMessage += GenHeader(3);
+  returnMessage += message;
+  returnMessage += "</body>";
+  returnMessage += "</html>";
+  server.send(200, "text/html", returnMessage);
+}
+void WebPage::handleFirmware() {
+  const char* serverIndex = 
+    "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+    "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+      "<input type='file' name='update'>"
+            "<input type='submit' value='Update'>"
+        "</form>"
+    "<div id='prg'>progress: 0%</div>"
+    "<script>"
+      "$('form').submit(function(e){"
+      "e.preventDefault();"
+      "var form = $('#upload_form')[0];"
+      "var data = new FormData(form);"
+      " $.ajax({"
+      "url: '/update',"
+      "type: 'POST',"
+      "data: data,"
+      "contentType: false,"
+      "processData:false,"
+      "xhr: function() {"
+      "var xhr = new window.XMLHttpRequest();"
+      "xhr.upload.addEventListener('progress', function(evt) {"
+      "if (evt.lengthComputable) {"
+      "var per = evt.loaded / evt.total;"
+      "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+      "}"
+      "}, false);"
+      "return xhr;"
+      "},"
+      "success:function(d, s) {"
+      "console.log('success!')" 
+    "},"
+    "error: function (a, b, c) {"
+    "}"
+    "});"
+    "});"
+    "</script>";
+  server.sendHeader("Connection", "close");
+  server.send(200, "text/html", serverIndex);
+}
+void WebPage::handleUpload() {
+  server.sendHeader("Connection", "close");
+  server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+  ESP.restart();
+}
+void WebPage::handleUpload2() {
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.printf("Update: %s\n", upload.filename.c_str());
+    if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    /* flashing firmware to ESP*/
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) { //true to set the size to the current progress
+      Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+    } else {
+      Update.printError(Serial);
+    }
+  }
+}
+void WebPage::handleRoot() {
+  String uri = DynamicData::get().ipaddress;
+  String message = "";
+  message += GenHeader(0);
+  
+  if( DynamicData::get().setNewNetwork == true )
+  {
+    message += "\t<form action=\"change\">\n";
+    message += "\t<label class=\"h2\" form=\"networkdata\">Network name</label>\n";
+    message += "\t<div>\n";
+    message += "\t<label for=\"netname\">netname</label>\n";
+    message += "\t<input type=\"text\" name=\"netname\" maxlength=\"30\">\n";
+    message += "\t</div>\n";
+    message += "\t<div>\n";
+    message += "\t<label for=\"password\">password</label>\n";
+    message += "\t<input type=\"text\" name=\"password\" maxlength=\"40\">\n";
+    message += "\t</div>\n";
+    message += "\t<div>\n";
+    message += "\t<button type=\"reset\">reset</button>\n";
+    message += "\t<button type=\"submit\">set</button>\n";
+    message += "\t</div>\n";
+    message += "\t</form>\n";
+  }
+  message += "\t<form action=\"change\">\n";
+  message += "\t<label class=\"h2\" form=\"scene\">set the stage </label>\n";
+  message += "\t<div>\n";
+  message += "\t<label for=\"scene\">scene</label>\n";
+  message += "\t<input type=\"text\"value=\"" + String(DynamicData::get().scene) + "\" name=\"scene\" maxlength=\"4\">\n";
+  message += "\t</div>\n";
+  message += "\t<div>\n";
+  message += "\t<label for=\"red\">red</label>\n";
+  message += "\t<input type=\"text\"placeholder=\"" + String(DynamicData::get().red) + "\" name=\"red\" maxlength=\"4\">\n";
+  message += "\t</div>\n";
+  message += "\t<div>\n";
+  message += "\t<label for=\"green\">green</label>\n";
+  message += "\t<input type=\"text\"placeholder=\"" + String(DynamicData::get().green) + "\" name=\"green\" maxlength=\"4\">\n";
+  message += "\t</div>\n";
+  message += "\t<div>\n";
+  message += "\t<label for=\"blue\">blue</label>\n";
+  message += "\t<input type=\"text\"placeholder=\"" + String(DynamicData::get().blue) + "\" name=\"blue\" maxlength=\"4\">\n";
+  message += "\t</div>\n";
+  message += "\t<div>\n";
+  message += "\t<button type=\"reset\">clear</button>\n";
+  message += "\t<button type=\"submit\">set</button>\n";
+  message += "\t</div>\n";
+  message += "\t</form>\n";
+
+  char timeh[5];
+  char timem[5];
+  char times[5];
+  message += "\t<p>---------------------------</p>\n";
+  message += "\t<p>ipaddress:  " + DynamicData::get().ipaddress + "</p>\n";
+  message += "\t<p>Thanks for trying my site mate. Have a good day!";
+  message += "\t<p>You have to always re set the RGB value (its only placeholders), otherwise it will reset to 0";
+  message += "\t<p>As mandated I have say a fair epilepsie warning for the scenes '6 & 7' ";
+  message += GenFooter();
+  server.send(200, "text/html", message);
+}
+
+String WebPage::addKeyValuePair(String key, String value)
+{
+  String retVal = "";
+  retVal += "\"";
+  retVal += key;
+  retVal += "\" :\"";
+  retVal += value;
+  retVal += "\"";
+  return retVal;
+}
+void WebPage::handleJson()
+{
+  String message = "";
+  String key = "";
+  message += "{ ";
+  
+  message += "}";
+  server.send(200, "text/plain", message);
+}
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,527 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClient.h>
+
+#include "WebPage.h"
+
+#include "NVMData.h"
+#include "DynamicData.h"
+
+WebPage webPage;
+
+#define PIN_WS2812B 2
+#define NUM_PIXELS 144
+
+CRGB leds[NUM_PIXELS];
+const int number = 143;
+int counter = 20;
+int scndcounter = 80;
+int thrdcounter = 120;
+int fourcounter = 40;
+int sevencounter = 60;
+int countereight = 100;
+const int c11 = 20;
+const int c12 = 80;
+const int c13 = 120;
+const int c14 = 40;
+const int c17 = 60;
+const int c18 = 100;
+int dirtybit = 142;
+int color1R = 0;
+int color1G = 0;
+int color1B = 255;
+int color2R = 255;
+int color2G = 0;
+int color2B = 0;
+int endcounter = 0;
+int secondEndTimer = 0;
+int oldScene = 0;
+bool counterended = true;
+bool counterhasended = false;
+bool counterTwoEnd = false;
+bool phase1 = false;
+
+//defined smth with these var.
+static const int delayTime_ms = 5;
+static const int SECONDS_1 = 200;
+
+void checkNetworkSet()
+{
+  if(DynamicData::get().setNewNetwork == true)
+  {
+    if(webPage.newNetworkSet == true)
+    {
+      DynamicData::get().setNewNetwork = false;
+      webPage.newNetworkSet = false;
+      NVMData::get().StoreNetData();
+    }
+  }
+  else
+  {
+    DynamicData::get().setNewNetwork = false;
+  }
+}
+
+void setupWiFi()
+{
+  String hostname = "LightStripAaron";
+  if (NVMData::get().NetDataValid() == false)
+  {
+    DynamicData::get().setNewNetwork = true;
+    Serial.println(WiFi.macAddress());
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(hostname.c_str(), "123Power");
+    delay(1000);
+    Serial.println(WiFi.softAPIP());
+    DynamicData::get().ipaddress = WiFi.softAPIP().toString();
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.setHostname(hostname.c_str());
+    Serial.printf("SSID = %s\n", NVMData::get().GetNetName().c_str());
+    WiFi.begin(NVMData::get().GetNetName().c_str(), NVMData::get().GetNetPassword().c_str());
+    Serial.println(WiFi.macAddress());
+    int maxWaitForNet = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(delayTime_ms);
+      if (maxWaitForNet < SECONDS_1)
+      {
+        maxWaitForNet++;
+      }
+      else
+      {
+        Serial.print(".");
+        WiFi.begin(NVMData::get().GetNetName().c_str(), NVMData::get().GetNetPassword().c_str());
+        DynamicData::get().incErrorCounter("Wifi startup");
+        maxWaitForNet = 0;
+      }
+    }
+    Serial.println("");
+    Serial.println("connected");
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.dnsIP());
+    DynamicData::get().ipaddress = WiFi.localIP().toString();
+  }
+}
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, PIN_WS2812B>(leds, NUM_PIXELS);
+  Serial.begin(115200);
+
+  //NVM data or like that as well as webpage smth
+  NVMData::get().Init();
+  DynamicData::get().Init();
+  Serial.println("NetName: "+  NVMData::get().GetNetName());
+  setupWiFi();
+  webPage.Init();
+}
+
+void twoPahseLights() {
+  if(phase1&&counterended){
+    leds[c12-5] = leds[c14-5] = leds[c18-5] = CRGB(color1R, color1G, color1B);
+    leds[c11-5] = leds[c13-5] = leds[c17-5] = CRGB(color2R,color2G,color2B);
+    phase1 = false;
+    counterended = false;
+    counterhasended = false;
+  }
+  
+  else if(!phase1&&counterended){
+    leds[c11] =  leds[c13]  = leds[c17] =  CRGB(color1R, color1G, color1B);
+    leds[c12] = leds[c14] = leds[c18] = CRGB(color2R,color2G,color2B);
+    phase1 = true;
+    counterended = false;
+    counterhasended = false;
+  }
+
+  if(counterhasended){
+    leds[c11] = leds[c12] = leds[c13] = leds[c14] = leds[c17] = leds[c18]  = CRGB::Black;
+    leds[c11-5] = leds[c12-5] = leds[c13-5] = leds[c14-5] = leds[c17-5] = leds[c18-5]  = CRGB::Black;
+  }
+
+  FastLED.show(); 
+  endcounter++;
+
+  if(endcounter == 249) {
+    counterhasended = true;
+  }
+
+  if(endcounter >=250) {
+    counterended = true;
+    endcounter = 0;
+  }
+}
+
+void trafficAdvisorWithFLashers() {
+  if(counter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else {
+    leds[counter-2] = CRGB::Black;
+  }
+
+  if(sevencounter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else {
+    leds[sevencounter-2] = CRGB::Black;
+  }
+
+  if(scndcounter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else {
+    leds[scndcounter-2] = CRGB::Black;
+  }
+
+  if(thrdcounter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else {
+    leds[thrdcounter-2] = CRGB::Black;
+  }
+
+  if(fourcounter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else {
+    leds[fourcounter-2] = CRGB::Black;
+  }
+  
+  if(counterTwoEnd) {
+    counter++;
+    scndcounter++;
+    thrdcounter++;
+    fourcounter++;
+    sevencounter++;
+  }
+
+  if(counterTwoEnd) {
+    leds[counter] = CRGB::OrangeRed;
+    leds[scndcounter] = CRGB::OrangeRed;
+    leds[counter-1] = CRGB::OrangeRed;
+    leds[scndcounter-1] = CRGB::OrangeRed;
+    leds[thrdcounter] = CRGB::OrangeRed;
+    leds[fourcounter] = CRGB::OrangeRed;
+    leds[sevencounter] = CRGB::OrangeRed;
+    leds[sevencounter-1] = CRGB::OrangeRed;
+    counterTwoEnd = false;
+  }
+
+  if(phase1) {
+    if(!counterended) {
+      leds[c11-5] = leds[c12-5] = leds[c18-5] = CRGB(color1R,color1G,color1B);
+      leds[c14-5] = leds[c13-5] = leds[c17-5] =  CRGB(color2R,color2G,color2B);
+    }
+    if(counterended) {
+      phase1 = false;
+      counterended = false;
+    }
+  }
+
+  else if(!phase1) {
+    if(!counterended) {
+       leds[c13] =  leds[c17] = leds[c14] =  CRGB(color1R,color1G,color1B);
+      leds[c11] = leds[c12] = leds[c18] = CRGB(color2R,color2G,color2B);
+    }
+    if(counterended) {
+      phase1 = true;
+      counterended = false;
+    }
+  }
+
+  if(counterhasended){
+    leds[c11] = leds[c12] = leds[c13] = leds[c14] = leds[c17] = leds[c18]  = CRGB::Black;
+    leds[c11-5] = leds[c12-5] = leds[c13-5] = leds[c14-5] = leds[c17-5] = leds[c18-5]  = CRGB::Black;
+    counterhasended = false;
+  }
+
+  FastLED.show();
+  
+  endcounter++;
+  secondEndTimer++;
+
+  if(secondEndTimer >= 6) {
+    counterTwoEnd = true;
+    secondEndTimer = 0;
+  }
+
+  if(endcounter == 249) {
+    counterhasended = true;
+  }
+
+  if(endcounter >=250) {
+    counterended = true;
+    endcounter = 0;
+  }
+
+  if(fourcounter >= 143) {
+    leds[fourcounter-1] = CRGB::Black;
+    fourcounter = 1;
+    leds[dirtybit-1] = CRGB::Black;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(sevencounter >= 143) {
+    leds[sevencounter-1] = CRGB::Black;
+    sevencounter = 1;
+    leds[dirtybit-1] = CRGB::Black;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(scndcounter >= 143) {
+    leds[scndcounter-1] = CRGB::Black;
+    scndcounter = 1;
+    leds[dirtybit-1] = CRGB::Black;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(thrdcounter >= 143) {
+    leds[thrdcounter-1] = CRGB::Black;
+    thrdcounter = 1;
+    leds[dirtybit-1] = CRGB::Black;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(counter >= 143) {
+    leds[counter-1] = CRGB::Black;
+    counter = 1;
+    leds[dirtybit-1] = CRGB::Black;
+    leds[dirtybit] = CRGB::Black;
+  }
+}
+
+void lightStripTrafficAdvisor() {
+  if(counter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else{
+    leds[counter-2] = CRGB::Black;
+  }
+
+  if(sevencounter < 2) {
+    leds[number] = CRGB::Black;
+  }
+
+  else{
+    leds[sevencounter-2] = CRGB::Black;
+  }
+
+  if(scndcounter < 2){
+    leds[number] = CRGB::Black;
+  }
+
+  else{
+    leds[scndcounter-2] = CRGB::Black;
+  }
+
+  if(thrdcounter < 2){
+    leds[number] = CRGB::Black;
+  }
+
+  else{
+    leds[thrdcounter-2] = CRGB::Black;
+  }
+
+  if(fourcounter < 2){
+    leds[number] = CRGB::Black;
+  }
+
+  else{
+    leds[fourcounter-2] = CRGB::Black;
+  }
+
+  
+  leds[counter] = CRGB::OrangeRed;
+  leds[scndcounter] = CRGB::OrangeRed;
+  leds[counter-1] = CRGB::OrangeRed;
+  leds[scndcounter-1] = CRGB::OrangeRed;
+  leds[thrdcounter] = CRGB::OrangeRed;
+  leds[fourcounter] = CRGB::OrangeRed;
+  leds[sevencounter] = CRGB::OrangeRed;
+  leds[sevencounter-1] = CRGB::OrangeRed;
+
+
+
+  delay(8);
+  FastLED.show();
+  counter++;
+  scndcounter++;
+  thrdcounter++;
+  fourcounter++;
+  sevencounter++;
+
+  if(fourcounter >= 143){
+    leds[fourcounter-1] = CRGB::Black;
+    fourcounter = 1;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(sevencounter >= 143){
+    leds[sevencounter-1] = CRGB::Black;
+    sevencounter = 1;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(scndcounter >= 143){
+    leds[scndcounter-1] = CRGB::Black;
+    scndcounter = 1;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(thrdcounter >= 143){
+    leds[thrdcounter-1] = CRGB::Black;
+    thrdcounter = 1;
+    leds[dirtybit] = CRGB::Black;
+  }
+
+  if(counter >= 143){
+    leds[counter-1] = CRGB::Black;
+    counter = 1;
+    leds[dirtybit] = CRGB::Black;
+  } 
+}
+
+void debugMultipuleLights(){
+  int colorR = 255; 
+  int colorB = 120;
+  int colorG = 100;
+  leds[20] = leds[40] = leds[60] = leds[80] = leds[100] = leds[120] = CRGB(colorR, colorG, colorB);
+  FastLED.show();
+}
+
+void warpCoreMode() {
+  leds[counter-1] = CRGB::Black;
+  leds[counter] = CRGB::White;
+  leds[sevencounter-1] = CRGB::Black;
+  leds[sevencounter] = CRGB::White;
+  FastLED.show();
+  if(counterended) {
+    counter++;
+    sevencounter++;
+    counterended = false;
+  }
+
+  endcounter++;
+  if(endcounter >= 3) {
+    counterended = true;
+    endcounter = 0;
+  }
+
+  if(counter > 143){
+    leds[counter-1] = CRGB::Black;
+    counter = 1;
+  }
+
+  if(sevencounter > 143){
+    leds[sevencounter-1] = CRGB::Black;
+    sevencounter = 1;
+  }
+}
+
+void fullLighting() {
+  fill_solid(leds, 100, CRGB(DynamicData::get().red, DynamicData::get().green, DynamicData::get().blue));
+  FastLED.show();
+}
+
+void FullLightFlash() {
+  if (counterhasended && !counterended) {
+    fill_solid(leds, 70, CRGB(DynamicData::get().red, DynamicData::get().green, DynamicData::get().blue));
+    counterhasended = false;
+    counterended = true;
+    FastLED.show();
+    Serial.println("on");
+  } 
+  else if (counterended && counterhasended) {
+    fill_solid(leds, 100, CRGB::Black);
+    counterended = false;
+    counterhasended = false;
+    FastLED.show();
+    Serial.println("off"); 
+  }
+
+  if (secondEndTimer >= 25) {
+    counterhasended = true;
+    secondEndTimer = 0;
+  }
+
+  secondEndTimer++;
+
+}
+
+void fillSolid80() {
+  fill_solid(leds, 80, CRGB(DynamicData::get().red, DynamicData::get().green, DynamicData::get().blue));
+  FastLED.show();
+}
+
+void offline() {
+  fill_solid(leds, 143, CRGB::Black);
+  FastLED.show();
+}
+
+void lighting() {
+  leds[20] = leds [60] = leds[100] = leds[140] = CRGB(DynamicData::get().red, DynamicData::get().green, DynamicData::get().blue);
+  FastLED.setBrightness(150);
+  FastLED.show();
+}
+
+// http://192.168.54.205/change?scene=4&red=0&green=0&blue=0 use that for changing scene and color (scene 4 (onyl soem spots) and 5 (full lighting) and case 6 (flashing in that color) only so far)
+void loop() {
+  checkNetworkSet();
+  webPage.loop();
+  if(oldScene != DynamicData::get().scene) {
+    offline();
+  }
+  oldScene = DynamicData::get().scene;
+  switch (DynamicData::get().scene)
+  {
+  case 0:
+    trafficAdvisorWithFLashers();
+    break;
+
+  case 1:
+    lightStripTrafficAdvisor();
+    break;
+
+  case 2:
+    twoPahseLights();
+    break;
+
+  case 3:
+    warpCoreMode();
+    break;
+  
+  case 4:
+    lighting();
+    break;
+  
+  case 5:
+    fullLighting();
+    break;
+  
+  case 6:
+    FullLightFlash();
+    break;
+
+  case 7:
+    fillSolid80();
+    break;
+
+  default:
+    offline();
+    break;
+  }
+
+  delay(1);
+  
+}
